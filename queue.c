fc797b3e6f98a592e3639a71c3e7d852e9114e68
Rispolyv0n <navy.horizon.314@gmail.com>
2019-02-21 22:03:32 +0800

    Fix q_new() and define the structure queue_t

--- queue.c
+++ queue.c
@@ -26,7 +26,9 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
-    q->head = NULL;
+    if (q) {
+        q->head = NULL;
+    }
     return q;
 }
 
