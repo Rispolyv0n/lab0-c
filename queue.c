2a541866e4d0ea8a9eea5a53dac6408a666917b4
Rispolyv0n <navy.horizon.314@gmail.com>
2019-02-22 11:28:07 +0800

    Add queue size calculating in q_remove_head()

--- queue.c
+++ queue.c
@@ -132,13 +132,16 @@ bool q_insert_tail(queue_t *q, char *s)
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* You need to fix up this code. */
-    if (q && q->head) {
+    if (q && q->size > 0) {
         strncpy(sp, q->head->value, bufsize - 1);
         sp[bufsize - 1] = '\0';
         list_ele_t *tmp = q->head;
         q->head = q->head->next;
         free(tmp->value);
         free(tmp);
+        q->size -= 1;
+        if (q->size == 0)
+            q->tail = NULL;
         return true;
     }
     return false;
